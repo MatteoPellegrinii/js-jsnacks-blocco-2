03b8c604d20aab086216f9f606955b15b199ae1f
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-12 15:31:56 +0200

    push

--- cinquanta/script.js
+++ cinquanta/script.js
@@ -1,13 +1,4 @@
-let myarray = [];
 
-while (myarray.length < 10) {
-  let random = Math.floor(Math.random()*100)+1;
-
-  if (!myarray.includes(random)) {
-    myarray.push(random);
-  }
-}
-console.log(myarray);
 
 
 
--- lavoro in classe 2/script.js
+++ lavoro in classe 2/script.js
@@ -1,13 +1,13 @@
-let array = [];
+let myarray = [];
 
-while (array.length < 10) {
-    let random = Math.floor(Math.random()* 100 + 1);
-    if (!array.includes(random)) {
-        array.push(random);
-    }
-    
-} 
-console.log(array);
+while (myarray.length < 10) {
+  let random = Math.floor(Math.random()*100)+1;
+
+  if (!myarray.includes(random)) {
+    myarray.push(random);
+  }
+}
+console.log(myarray);
 
 
 
