d2cdd2fdc6c10a245efaffc07f8cbd8e14cc5496
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-12 15:37:52 +0200

    fine

--- cinquanta/script.js
+++ cinquanta/script.js
@@ -1,4 +1,16 @@
+let arraynumeriutent = [];
+let sommanumeriutente = 0;
 
+do {
+
+  let numeroutente = parseInt(prompt("inserisci un numero"));
+  arraynumeriutent.push(numeroutente);
+
+  sommanumeriutente += numeroutente;
+
+} while (sommanumeriutente < 50)
+
+console.log(arraynumeriutent);
 
 
 
